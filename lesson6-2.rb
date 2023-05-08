33734fd358e46f38892f6512904e006c60bbbd44
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-08 12:55:04 +0000

    [finish]lesson6-1

---
+++ lesson6-2.rb
@@ -0,0 +1,12 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+n=gets.to_i
+for n in 1..10 do
+  puts "#{n}回目の計算"
+  puts "2つの値を入力してください"
+  あ=gets.to_i
+  い=gets.to_i
+  puts "#{あ}"
+  puts "#{い}"
+  puts "計算結果を出力します"
+end
